d2f7522f9ad8ee1e6dcb118fb2639c091716ee7b
PaulosET <abebawpaulos@gmail.com>
2024-02-21 19:15:20 +0530

    simple blog mern website

---
+++ Client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import Signin from './pages/Signin'
+import Project from './pages/Project'
+import Dashboard from './pages/Dashboard'
+export default function App() {
+  return (
+     <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/sign-in" element={<Signin/>}/>
+      <Route path="/sign-up" element={<SignUp/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/projects" element={<Project/>}/>
+
+
+     </Routes>
+     
+     </BrowserRouter>
+  )
+}
