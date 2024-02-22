36f2a581a1b6949f2ce21939ab48061ddc53aeec
PaulosET <abebawpaulos@gmail.com>
2024-02-22 18:46:06 +0530

    navbar done

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,9 +6,11 @@ import SignUp from './pages/SignUp'
 import Signin from './pages/Signin'
 import Project from './pages/Project'
 import Dashboard from './pages/Dashboard'
+import Header from './components/Header'
 export default function App() {
   return (
      <BrowserRouter>
+     <Header/>
      <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/about" element={<About />}/>
--- Client/src/pages/Signin.jsx
+++ Client/src/pages/Signin.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Signin() {
   return (
     <div>
-      
+      <h1>sign in page</h1>
     </div>
   )
 }
